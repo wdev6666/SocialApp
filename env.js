8350cdc5b7d4790db310d435af3c12b791b8e9ca
n.eth <wdev6666@gmail.com>
2023-09-01 09:22:46 +0530

    Start

---
+++ env.js
@@ -0,0 +1,6 @@
+const vars = {
+    apiUrl: "https://socialapp-serverr.herokuapp.com",
+    defaultImageUri: "https://socialapp-serverr.herokuapp.com/static/images/defaultprofile.jpg"
+};
+
+export default vars;
